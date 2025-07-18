3685c8c37d736496f8fd1855f381d8fd97402d5e
Shivanshu Patel <shivanshupatel2004@gmail.com>
2025-07-18 17:33:02 +0530

    Changes in api

--- leaderboard/vite.config.js
+++ leaderboard/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://leaderboard-task-9.onrender.com', 
+        target: 'http://localhost:5000', 
         changeOrigin: true,
         secure: false,
       },
